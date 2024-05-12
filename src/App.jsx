1a34f1b22f3f93f2f6d0adf417ae400f8f13530a
NeumanGallardo <neuman10327033@gmail.com>
2024-05-12 18:20:50 -0300

    formLoginP1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import CreateSuplement from './components/CreateSuplements/index';
+import Login from './components/Login/index';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,6 +11,7 @@ function App() {
     <>
 <Routes>
 <Route path='createsuplements' element={<CreateSuplement/>}/>
+<Route path='login' element={<Login/>}/>
 </Routes>
     </>
   )
